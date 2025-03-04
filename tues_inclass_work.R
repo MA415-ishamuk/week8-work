a7b8e4b797f1f4b9abaaecb6a7502476ae4823ec
ishamuk <ishamuk@bu.edu>
2025-03-04 15:09:49 -0500

    notes added from tuesday (3/4) class introducing exploratory data analysis project

---
+++ tues_inclass_work.R
@@ -0,0 +1,15 @@
+###
+# notes from class
+###
+
+# can use quick stats through USDA to develop data sets picking the variable that
+# you are particularly interested in --> strawberry, chemicals, economics, etc
+
+# when writing make a file called myfunctions.R and list all the used functions 
+# there then use source("myfunctions.R") to access those functions from elsewhere'
+
+
+# start of code that can be used to organize the chemicals used
+# chemical_counts <- survey_chem %>%
+#   group_by(`Chemical Name`) %>%
+  
