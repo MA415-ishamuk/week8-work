6bd6a9b46465845ea09608e8fd481500df667fbb
ishamuk <ishamuk@bu.edu>
2025-03-18 13:58:50 -0400

    notes from week 8 discussion

--- tues_inclass_work.R
+++ tues_inclass_work.R
@@ -12,4 +12,17 @@
 # start of code that can be used to organize the chemicals used
 # chemical_counts <- survey_chem %>%
 #   group_by(`Chemical Name`) %>%
-  
+
+library(kableExtra)
+
+chemical_counts %>%
+  kable("html", caption = "Chemical Counts by Chemical Name") %>%
+  kable_styling("striped", full_width = False) %>%
+  column_spec(1, bold = True, color = "blue") %>%
+  column_spec(2, background = "#f2f2f2") %>%
+  row_spec(0, bold = True, font_size = 14)
+
+# can also add headers to the table
+# add_header_above()
+# "hover" -> under kable_styling which shows around where your cursor is 
+# can also use datatable()
